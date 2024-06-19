59005ece56d261309cd85bbcfe1585ea6c18dc17
Drakayshin <96027721+Drakayshin@users.noreply.github.com>
2024-06-19 19:07:39 -0400

    SAO 19/06/2024
    * c154/Designio de Represalia
    *c128/Rokunio Bestial

--- script/c128.lua
+++ script/c128.lua
@@ -2,32 +2,34 @@
 --Rokunio Bestial
 local s,id=GetID()
 function s.initial_effect(c)
-	--Sustituto de Material
+	-- Sustituto de Material
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_FUSION_SUBSTITUTE)
 	e1:SetCondition(s.subcon)
 	c:RegisterEffect(e1)
-    --Invocar por por Fusion 1 Monstruo Fusion "Bestial"
+    -- Invocar por por Fusion 1 Monstruo Fusion "Bestial"
 	local params={function(c) return c:IsSetCard(0x3e9) end}
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_BATTLE_START)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMING_MAIN_END)
 	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function() return Duel.IsMainPhase() end)
 	e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
 	e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
 	c:RegisterEffect(e2)
-    --Invocar desde el Cementerio
+    -- Invocar desde el Cementerio
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCountLimit(1,id)
+	e3:SetCondition(aux.exccon)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
@@ -36,7 +38,7 @@ s.listed_series={0x3e9}
 function s.subcon(e)
 	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_REMOVED+LOCATION_GRAVE)
 end
-    --Invocar desde el Cementerio
+    -- Invocar desde el Cementerio
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
@@ -44,7 +46,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-        --Barajalo a tu Deck cuando deje el Campo
+        -- Barajalo a tu Deck cuando deje el Campo
         local e2=Effect.CreateEffect(e:GetHandler())
         e2:SetDescription(3900)
 		e2:SetType(EFFECT_TYPE_SINGLE)
