b4194ffb981c2d92ae1135124074326b3011effa
Drakayshin <96027721+Drakayshin@users.noreply.github.com>
2025-04-24 11:49:28 -0400

    Actualización 24/04/2025

---
+++ script/c185.lua
@@ -0,0 +1,86 @@
+--La Veleidad del Caos
+--DrakayStudios
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Robar 2 cartas, con posible busqueda
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_DRAW+CATEGORY_SEARCH)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e0:SetCost(s.cost)
+	e0:SetTarget(s.target)
+	e0:SetOperation(s.activate)
+	c:RegisterEffect(e0)
+	-- Añadir 1 "Dualidad" or "Luz de la Sombra" desde el Deck o Cementerio
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(aux.exccon)
+	e1:SetCost(Cost.SelfBanish)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_CHAOS,0x10cf}
+	-- Robar 2 cartas, con posible busqueda
+function s.costfilter(c)
+	return (c:IsAttribute(ATTRIBUTE_DARK) or c:IsAttribute(ATTRIBUTE_LIGHT)) and c:IsDiscardable()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.filter(c)
+	return c:IsType(TYPE_MONSTER) and (c:IsSetCard(SET_CHAOS) or c:IsSetCard(0x10cf)) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+    if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+        if #g>0 then
+			Duel.BreakEffect()
+            Duel.SendtoHand(g,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,g)
+        end
+    end
+	-- Limitar a Invocar de Modom Especial monstruos de Luz o Oscuridad desde el Deck Extra
+	local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(s.splimit)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+end
+function s.splimit(e,c)
+	return c:IsLocation(LOCATION_EXTRA) and not  (c:IsSetCard(0x10cf) or c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_DARK))
+end
+-- Añadir 1 "Dualidad" or "Luz de la Sombra" desde el Deck o Cementerio
+function s.gythfilter(c)
+	return (c:IsCode(39973386) or c:IsCode(61322713)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.gythfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.gythfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
