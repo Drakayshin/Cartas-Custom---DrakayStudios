35c7058929e1e99b817fac0b48fc009db5a9ad9c
Drakayshin <96027721+Drakayshin@users.noreply.github.com>
2025-08-07 17:42:35 -0400

    Actualización: 07/08/2025
    Cambios Internos:
    *	Errata - Disebia, Consorte Terranigma
    Cartas Añadidas:
    *	Aveniencia Terranigma

---
+++ script/c80.lua
@@ -0,0 +1,103 @@
+--Aveniencia Terranigma
+--DrakayStudios
+local s,id=GetID()
+function s.initial_effect(c)
+    --  0° Activar desde la mano si controlas un monstruo "Terranigma" de nivel 7 o mayor
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e0:SetCondition(function(e) return Duel.IsExistingMatchingCard(s.actfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil) end)
+	c:RegisterEffect(e0)
+	--  1° Tomar el control de un monstruo Invocado de Modo Especial por tu adversario
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+    --  2° Añadir a tu mano 1 monstruo "Terranigma" desde el Deck o Cementerio y luego poder Invocar de Modo Normal 1 monstruo "Terranigma"
+    local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_CHAIN_NEGATED)
+	e2:SetRange(LOCATION_GRAVE|LOCATION_REMOVED)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e2:SetCondition(s.thcond)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	local e2a=e2:Clone()
+	e2a:SetCode(EVENT_DESTROYED)
+	e2a:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp and r&REASON_EFFECT>0 end)
+    c:RegisterEffect(e2a)
+end
+s.listed_series={0x3e7}
+    --  *EFECTO 0°
+function s.actfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x3e7) and c:IsMonster()
+end
+    --  *EFECTO 1°
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	if chk==0 then return tc:IsAbleToChangeControler() end
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,eg,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+    if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+        Duel.GetControl(tc,tp)
+		--  *Destierralo si deja el Campo
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3300)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+		tc:RegisterEffect(e1,true)
+		Duel.BreakEffect()
+	end
+end
+    --  *EFECTO 2°
+function s.thcond(e,tp,eg,ep,ev,re,r,rp)
+	local de,dp=Duel.GetChainInfo(ev,CHAININFO_DISABLE_REASON,CHAININFO_DISABLE_PLAYER)
+	return re:GetHandler()==e:GetHandler() and re:IsHasType(EFFECT_TYPE_ACTIVATE) and rp==tp and de and dp~=tp
+end
+function s.filter1(c)
+	return c:IsSetCard(0x3e7) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+end
+function s.sumfilter(c)
+	return c:IsSetCard(0x3e7) and c:IsSummonable(true,nil)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		if not g:GetFirst():IsLocation(LOCATION_HAND) then return end
+		-- *Invocar de Modo Normal
+		local sg1=Duel.GetMatchingGroup(s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
+		if #sg1>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.BreakEffect()
+			Duel.ShuffleHand(tp)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+			local sg2=sg1:Select(tp,1,1,nil):GetFirst()
+			Duel.Summon(tp,sg2,true,nil)
+		end
+	end
+end
--- script/c93.lua
+++ script/c93.lua
@@ -2,75 +2,92 @@
 --DrakayStudios
 local s,id=GetID()
 function s.initial_effect(c)
-	--	0° Inegable activación y efectos de cartas "Terranigma"
+	-- 	0° No puede ser Invocado de Modo Especial
 	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetCode(EFFECT_CANNOT_INACTIVATE)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetValue(s.effectfilter)
-	c:RegisterEffect(e0)
-	local e0a=Effect.CreateEffect(c)
-	e0a:SetType(EFFECT_TYPE_FIELD)
-	e0a:SetCode(EFFECT_CANNOT_DISEFFECT)
-	e0a:SetRange(LOCATION_MZONE)
-	e0a:SetValue(s.effectfilter)
-    c:RegisterEffect(e0a)
-    --	1° Tu adversario no puede selecciar esta carta para taques o por efectos de cartas
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+    c:RegisterEffect(e0)
+	--	1° Inegable activación y efectos de cartas "Terranigma"
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_INACTIVATE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.con)
-	e1:SetValue(aux.imval2)
+	e1:SetValue(s.effectfilter)
 	c:RegisterEffect(e1)
-	local e1a=e1:Clone()
-	e1a:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e1a:SetValue(aux.tgoval)
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_FIELD)
+	e1a:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e1a:SetRange(LOCATION_MZONE)
+	e1a:SetValue(s.effectfilter)
 	c:RegisterEffect(e1a)
-    -- 	2 Gana ATK/DEF por cada monstruo de Oscuridad en el Campo
+	local e1b=Effect.CreateEffect(c)
+	e1b:SetType(EFFECT_TYPE_FIELD)
+	e1b:SetCode(EFFECT_CANNOT_DISABLE)
+	e1b:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1b:SetRange(LOCATION_MZONE)
+	e1b:SetTargetRange(LOCATION_ONFIELD,0)
+	e1b:SetValue(s.indtg)
+    c:RegisterEffect(e1b)
+    --	2° Tu adversario no puede selecciar esta carta para taques o por efectos de cartas
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(s.atkval)
+	e2:SetCondition(s.con)
+	e2:SetValue(aux.imval2)
 	c:RegisterEffect(e2)
 	local e2a=e2:Clone()
-	e2a:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e2a)
-    -- 	3° Invocar de Modo Normal 1 monstruo "Terranigma"
+	e2a:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2a:SetValue(function(e,te) return te:GetOwnerPlayer()~=e:GetHandlerPlayer() end)
+	c:RegisterEffect(e2a)
+    -- 	3° Gana 500 ATK/DEF por cada monstruo de Oscuridad en el Campo
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_SUMMON)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetHintTiming(TIMINGS_CHECK_MONSTER|TIMING_MAIN_END|TIMING_DAMAGE_STEP)
-	e3:SetCountLimit(1)
-	e3:SetTarget(s.sumtg)
-	e3:SetOperation(s.sumop)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
+	local e3a=e3:Clone()
+	e3a:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e3a)
+    -- 	4° Invocar de Modo Normal 1 monstruo "Terranigma"
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_SUMMON)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetHintTiming(TIMINGS_CHECK_MONSTER|TIMING_MAIN_END|TIMING_DAMAGE_STEP)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.sumtg)
+	e4:SetOperation(s.sumop)
+	c:RegisterEffect(e4)
 end
 s.listed_series={0x3e7}
-	-- 	*EFECTO 0°
+	-- 	*EFECTO 1°
 function s.effectfilter(e,ct)
 	local p=e:GetHandler():GetControler()
 	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
 	return p==tp and te:GetHandler():IsSetCard(0x3e7) and loc&LOCATION_ONFIELD|LOCATION_HAND~=0
 end
-    -- 	*EFECTO 1°
+function s.indtg(e,c)
+	return c:IsSetCard(0x3e7) and (c:IsSpellTrap() or c:IsMonster())
+end
+    -- 	*EFECTO 2°
 function s.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x3e7) and not c:IsCode(id)
 end
 function s.con(e)
 	return Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
-    -- 	*EFECTP 2°
+    -- 	*EFECTP 3°
 function s.atkval(e,c)
 	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_DARK),c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,0,nil)*500
 end
-    -- 	*EFECTO 3°
+    -- 	*EFECTO 4°
 function s.sumfilter(c)
 	return c:IsSetCard(0x3e7) and c:IsSummonable(true,nil)
 end
