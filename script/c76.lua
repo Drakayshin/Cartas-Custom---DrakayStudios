ebe0f34c98e909b5e1e4056ce926d69c6dcaff40
Drakayshin <96027721+Drakayshin@users.noreply.github.com>
2025-08-24 16:53:35 -0400

    Add files via upload
    Cambios Internos:
    *	Errata - Adadon, El Terranigma Omega

--- script/c76.lua
+++ script/c76.lua
@@ -11,7 +11,8 @@ function s.initial_effect(c)
     e0:SetDescription(aux.Stringid(id,0))
     e0:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
 	e0:SetType(EFFECT_TYPE_IGNITION)
-    e0:SetRange(LOCATION_HAND)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCost(s.spcost)
     e0:SetCondition(s.spcon)
     e0:SetTarget(s.sptg)
 	e0:SetOperation(s.spop)
@@ -32,10 +33,20 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_names={id}
-    --  *EFECTO 0°
+	--  *EFECTO 0°
+function s.cfilter(c,ft)
+	return c:IsFaceup() and c:IsSetCard(0x3e7) and c:IsAbleToHandAsCost() and (ft>0 or c:GetSequence()<5)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,ft) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,ft)
+	Duel.SendtoHand(g,nil,REASON_COST)
+end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-	return (#g==0 or (#g>0 and g:FilterCount(aux.FaceupFilter(Card.IsSetCard,0x3e7),nil)==#g))
+	return (#g>0 and g:FilterCount(aux.FaceupFilter(Card.IsSetCard,0x3e7),nil)==#g)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -47,6 +58,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
 		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+		and not Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,0,1,e:GetHandler()) 
 		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
@@ -58,13 +70,14 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
     --  *EFECTO 1°
-function s.costfilter(c)
+function s.costfilter(c,tp)
 	return c:IsMonster() and c:IsSetCard(0x3e7) and c:IsAbleToGraveAsCost()
+	and not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,c:GetCode()),tp,LOCATION_GRAVE,0,1,nil)
 end
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_DECK,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
