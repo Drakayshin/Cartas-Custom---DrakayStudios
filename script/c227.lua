3f61796de4967f935ad0f21ffaf906ec9fcc27e2
Drakayshin <96027721+Drakayshin@users.noreply.github.com>
2025-09-21 16:42:13 -0400

    Actualización: 21/09/2025
    Cambios Internos:
    *	Dirreción general 228 -> 227
    *	Dirreción general 227 -> 228
    
    Cartas Añadidas:
    *	Solarsemilla Minus Samsara

--- script/c227.lua
+++ script/c227.lua
@@ -1,107 +1,145 @@
---Solarvid Treant
+--Solarsemilla Minus Samsara
 --DrakayStudios
 local s,id=GetID()
 function s.initial_effect(c)
-    --  *Invocación por Enlace
-	c:EnableReviveLimit()
-    Link.AddProcedure(c,nil,1,2,s.lcheck)
-    --  0° Destruir esta carta si un Monstruo de Enlace "Solaravalon" deja el Campo por efecto de una carta
+    -- 	0° Tratar esta carta como Normal en tu Mano
     local e0=Effect.CreateEffect(c)
-	e0:SetCategory(CATEGORY_DESTROY)
-	e0:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetCode(EVENT_LEAVE_FIELD)
-	e0:SetCondition(s.descon)
-	e0:SetTarget(s.destg)
-	e0:SetOperation(s.desop)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e0:SetCode(EFFECT_ADD_TYPE)
+	e0:SetValue(TYPE_NORMAL)
 	c:RegisterEffect(e0)
-    --  1° Invocar de Modo Especial 1 Monstruo de Enlace Planta en el Cementerio
+	local e0a=e0:Clone()
+	e0a:SetCode(EFFECT_REMOVE_TYPE)
+	e0a:SetValue(TYPE_EFFECT)
+	c:RegisterEffect(e0a)
+    --  1° Invocar de Modo Especial a esta carta y, despúes puedes ya sea Invocar de Modo Especial o añadir a tu mano 1 monstruo
     local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(function(e) return e:GetHandler():IsLinkSummoned() end)
+	e1:SetCode(EVENT_TO_HAND)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(function(e) return not e:GetHandler():IsReason(REASON_DRAW) end)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-    --  2° Negar efectos de monstruos que batallen contra monstruos "Solarvid" o "Solaravalon"
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_DISABLE)
-	e2:SetRange(LOCATION_MZONE)
-    e2:SetTargetRange(0,LOCATION_MZONE)
-    e2:SetCondition(function(e) return e:GetHandler():IsLinked() end)
-	e2:SetTarget(s.distg)
+    -- 2° Añadir efectos a un Monstruo de Enlace Invocado usando a esta carta como material
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return (r&REASON_LINK)==REASON_LINK and e:GetHandler():GetReasonCard():IsSetCard(SET_SUNAVALON) end)
+	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
-	--  2° Aplicar negación de efectos
-	local e2a=Effect.CreateEffect(c)
-	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2a:SetCode(EVENT_BE_BATTLE_TARGET)
-	e2a:SetRange(LOCATION_MZONE)
-	e2a:SetOperation(function(e) Duel.AdjustInstantly(e:GetHandler()) end)
-	c:RegisterEffect(e2a)
 end
-s.listed_series={SET_SUNAVALON,SET_SUNVINE,0x4157}
-    --  *Filtro de Materiales
-function s.lcheck(g,lc,sumtype,tp)
-	return g:IsExists(Card.IsRace,1,nil,RACE_PLANT,lc,sumtype,tp)
-end
-    --  *EFECTO 0°
-function s.cfilter(c,tp,rp)
-	return c:IsPreviousPosition(POS_FACEUP) and c:IsSetCard(SET_SUNAVALON) and c:IsType(TYPE_LINK) 
-		and c:GetPreviousControler()==tp and c:IsReason(REASON_EFFECT)
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp,rp)
-end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.Destroy(c,REASON_EFFECT)
-	end
-end
-    --  *EFECTO 1°
-function s.spfilter(c,e,tp,zone)
-	return c:IsSetCard(SET_SUNVINE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
-end
+s.listed_series={SET_SUNAVALON,SET_SUNVINE}
+s.listed_names={27520594} --Nombre de Carta a convertir o usar
+    -- 	*EFECTO 2°
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,e:GetHandler():GetLinkedZone(tp)) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+    Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local zone=c:GetLinkedZone(tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)
-	if c:IsRelateToEffect(e) and c:IsFaceup() and zone>0 and ft>0 then
-		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,ft,ft,nil,e,tp,zone)
-		if #g>0 then
-			local ct=Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP,zone)
-			if ct>0 then
-				c:UpdateAttack(ct*1200)
-			end
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		--  *Invocar de Modo Especial 1 monstruo "Solarvid" o añadirlo a tu mano
+        local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp,ft) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+			local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp,ft):GetFirst()
+			if not sc then return end
+			Duel.BreakEffect()
+			aux.ToHandOrElse(sc,tp,
+				function()
+					return ft>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+				end,
+				function()
+					Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+				end,
+				aux.Stringid(id,1)
+			)
 		end
 	end
 end
-    --  *EFECTO 2°
-function s.distg(e,c)
-	local fid=e:GetHandler():GetFieldID()
-	for _,label in ipairs({c:GetFlagEffectLabel(id)}) do
-		if fid==label then return true end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_SUNVINE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+    --  *EFECTO 3°
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	if not rc:IsType(TYPE_EFFECT) then
+		--It becomes an Effect Monster if it's not one already
+		local e0=Effect.CreateEffect(c)
+		e0:SetType(EFFECT_TYPE_SINGLE)
+		e0:SetCode(EFFECT_ADD_TYPE)
+		e0:SetValue(TYPE_EFFECT)
+		e0:SetReset(RESET_EVENT|RESETS_STANDARD)
+		rc:RegisterEffect(e0,true)
 	end
-	local bc=c:GetBattleTarget()
-	if c:IsRelateToBattle() and bc and bc:IsControler(e:GetHandlerPlayer())
-		and bc:IsFaceup() and bc:IsSetCard({SET_SUNAVALON,SET_SUNVINE}) then
-		c:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1,fid)
-		return true
+	--  *Evitar se afectado por efectos de otras cartas por este turno
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.immop)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+	--	*Ganar LP igual al ATK de todos los monstruos con el mismo tipo
+	local e2=e1:Clone()
+	e2:SetDescription(aux.Stringid(id,3))
+    e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_NO_TURN_RESET)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
+    rc:RegisterEffect(e2,true)
+end
+    --  *EFECTO 3° (1°)
+function s.immop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		--Unaffected by other card effects
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3100)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetValue(s.efilter)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		c:RegisterEffect(e1)
 	end
-	return false
+end
+function s.efilter(e,re)
+	return re:GetOwner()~=e:GetOwner() and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+end
+    --  *EFECTO 3° (2°)
+function s.rfilter(c,race)
+	return c:IsFaceup() and c:IsRace(race)
+end
+function s.tfilter(c)
+	return c:IsFaceup()
+		and Duel.GetMatchingGroup(s.rfilter,0,LOCATION_MZONE,LOCATION_MZONE,nil,c:GetRace()):GetSum(Card.GetAttack)>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELF)
+	local g=Duel.SelectTarget(tp,s.tfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local val=Duel.GetMatchingGroup(s.rfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,g:GetFirst():GetRace()):GetSum(Card.GetAttack)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,val)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local val=Duel.GetMatchingGroup(s.rfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tc:GetRace()):GetSum(Card.GetAttack)
+		Duel.Recover(tp,val,REASON_EFFECT)
+	end
 end
