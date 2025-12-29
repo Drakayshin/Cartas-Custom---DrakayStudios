a7930a4577a875c82b376ffba228313aaf5e93aa
Drakay <96027721+Drakayshin@users.noreply.github.com>
2025-12-29 22:51:13 +0100

    Add files via upload

--- script/c259.lua
+++ script/c259.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
     e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
     e0:SetType(EFFECT_TYPE_ACTIVATE)
     e0:SetCode(EVENT_FREE_CHAIN)
-    e0:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+    e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
     e0:SetOperation(s.activate)
     c:RegisterEffect(e0)
     --  1° Reducir el daño a 0 de batallas espeficias
