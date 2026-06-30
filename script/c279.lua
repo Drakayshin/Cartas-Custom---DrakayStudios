27281982605a6ad0a11d017adcfb4750a64e3371
Drakay <96027721+Drakayshin@users.noreply.github.com>
2026-06-30 16:59:57 -0400

    Actualización: 30/06/2026
    Cambios Internos:
    ●	Script - c134
    
    Cartas Añadidas:
    ●	Rastreador de Disformidad

--- script/c279.lua
+++ script/c279.lua
@@ -1,110 +1,125 @@
---Elementor Oscuro
+--Rastreador de Disformidad
 --DrakayStudios
 local s,id=GetID()
 function s.initial_effect(c)
-    --  0° No puedes Invocar de Modo Especial desde tu Deck Extra
+	--	Efecto 0: Reducir daño por batalla
 	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
 	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e0:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e0:SetRange(LOCATION_MZONE)
-	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e0:SetTargetRange(1,0)
-	e0:SetTarget(function(e,c,sump,sumtype,sumpos,targetp)return c:IsLocation(LOCATION_EXTRA)end)
+	e0:SetTargetRange(LOCATION_MZONE,0)
+	e0:SetTarget(function(e,c) return c:IsAttribute(ATTRIBUTE_DARK) end)
+	e0:SetValue(1)
 	c:RegisterEffect(e0)
-	--  *auxiliar de efecto continuo
-    aux.addContinuousLizardCheck(c,LOCATION_MZONE)
-    --  1° Reflejo de daño por batalla a tu adversario
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
-	e1:SetValue(1)
-    c:RegisterEffect(e1)
-    --  2° Invocar a esta carta de Modo Especial (desde la mano)
-    local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetRange(LOCATION_HAND)
-	e2:SetCondition(s.spcon)
+	--  Efecto 1: Invocar de Modo Especial a esta carta desde tu mano
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(0,TIMING_MAIN_END|TIMING_BATTLE_START|TIMING_BATTLE_END|TIMINGS_CHECK_MONSTER_E)
+    e1:SetCountLimit(1,{id,0})
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--	Efecto 2: 
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,4))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetOperation(s.spop1)
 	c:RegisterEffect(e2)
-    --  3° Invocar a esta carta de Modo Especial desde el Cementerio o destierro
-    local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_GRAVE|LOCATION_REMOVED)
-    e3:SetCountLimit(1,{id,0})
-    e3:SetCondition(function(_,tp) return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,100),tp,LOCATION_ONFIELD,0,1,nil) end)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
-	c:RegisterEffect(e3)
-	--  4° Añadir a tu mano o Invocar de Modo Especial hasta 2 "Molestor"
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_TOHAND|CATEGORY_SEARCH|CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-    e4:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e4:SetCountLimit(1,{id,1})
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
-	c:RegisterEffect(e4)
-	local e4a=e4:Clone()
-	e4a:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e4a)
+	local e2a=e2:Clone()
+    e2a:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2a)
+    local e2b=e2:Clone()
+    e2b:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2b)
 end
-s.listed_names={100}
-    --  *EFECTO 2°
-function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
-	and (Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 
-	or not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_WATER|ATTRIBUTE_EARTH|ATTRIBUTE_FIRE|ATTRIBUTE_WIND|ATTRIBUTE_LIGHT),tp,LOCATION_MZONE,0,1,nil))
+s.listed_names={100,105}
+local LOCATION_HAND_GRAVE_REMOVED=LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED
+	--	*EFECTO 1°
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return (Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)) or 
+	Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,100),tp,LOCATION_MZONE,0,1,nil)
 end
-    --  *EFECTO 3°
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-    and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SET,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(3900)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-		e1:SetValue(LOCATION_DECKSHF)
-		e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
-        c:RegisterEffect(e1,true)
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
+		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+		local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.setfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil):GetFirst()
+		if sc and Duel.SSet(tp,sc)>0 then
+			--It can be activated this turn
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(aux.Stringid(id,3))
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_CLIENT_HINT)
+			e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+			e1:SetReset(RESETS_STANDARD_PHASE_END)
+			sc:RegisterEffect(e1)
+		end
 	end
 end
-	--	*EFECTO 4°
-function s.adfilter(c,e,tp,octagonshiri_chk)
-	return c:IsCode(id) and (c:IsAbleToHand() or (octagonshiri_chk and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+function s.setfilter(c)
+	return c:IsCode(105) and c:IsSSetable()
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local octagonshiri_chk=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,100),tp,LOCATION_ONFIELD,0,1,nil)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.adfilter,tp,LOCATION_DECK,0,1,nil,e,tp,octagonshiri_chk) end
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	--	*EFECTO 2°
+function s.spfilter2(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK) and (c:IsFaceup() or c:IsLocation(LOCATION_HAND)) and
+	((Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) or
+	(Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)))
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local octagonshiri_chk=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,100),tp,LOCATION_ONFIELD,0,1,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
-	local sc=Duel.SelectMatchingCard(tp,s.adfilter,tp,LOCATION_DECK,0,1,2,nil,e,tp,octagonshiri_chk)
-	if not sc then return end
-	aux.ToHandOrElse(sc,tp,
-		function(sc)
-			return octagonshiri_chk and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		end,
-		function(sc)
-			return Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
-		end,
-		aux.Stringid(id,2)
-	)
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND_GRAVE_REMOVED,0,1,nil,e,tp) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_HAND_GRAVE_REMOVED,0,1,1,nil,e,tp):GetFirst()
+		if not sc then return end
+		local b3=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		local b4=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)
+		if not (b3 or b4) then return end
+		local op2=Duel.SelectEffect(tp,{b3,aux.Stringid(id,5)},{b4,aux.Stringid(id,6)})
+		local target_player2=op2==1 and tp or 1-tp
+		Duel.BreakEffect()
+		--	*Invocar el monstruo y esperar a terminar la Invocación
+		Duel.SpecialSummonStep(sc,0,tp,target_player2,false,false,POS_FACEUP)
+		--	*No puede cambiar su posición de batalla
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3313)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		sc:RegisterEffect(e1)
+		--	*Debe atacar de ser posible
+		local e2=e1:Clone()
+		e2:SetDescription(3200)
+		e2:SetCode(EFFECT_MUST_ATTACK)
+		sc:RegisterEffect(e2)
+		--	*Barajar al Deck cuando deje el Campo
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetDescription(3900)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e3:SetValue(LOCATION_DECKSHF)
+		e3:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		sc:RegisterEffect(e3)
+		--	*Complemtar Invocación
+		Duel.SpecialSummonComplete()
+	end
 end
