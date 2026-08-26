611d8cf5f328ade809f7e5cebe3d54141e8aff62
Drakay <96027721+Drakayshin@users.noreply.github.com>
2026-08-25 22:07:04 -0400

    Actualización: 25/08/2026
    ●	Cartas Añadidas:
    ● Shicard, Arcano de la Fábula

--- script/c300.lua
+++ script/c300.lua
@@ -7,7 +7,9 @@ function s.initial_effect(c)
 	local synchro_proc0=Synchro.AddProcedure(c,nil,2,99,aux.FilterSummonCode(290),1,1)
 	local synchro_proc1=Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),1,1,aux.FilterSummonCode(290),1,1)
 	synchro_proc0:SetDescription(aux.Stringid(id,0))
-	synchro_proc1:SetDescription(aux.Stringid(id,1))
+    synchro_proc1:SetDescription(aux.Stringid(id,1))
+    --  *Debe ser invocado por Sincronía
+    c:AddMustBeSynchroSummoned()
 	--  Efecto 0: El nombre de esta carta se convierte en "Yacard, Héroe de la Fábula" en Campo o Cementerio
     local e0=Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_SINGLE)
@@ -16,48 +18,57 @@ function s.initial_effect(c)
     e0:SetRange(LOCATION_MZONE|LOCATION_GRAVE)
     e0:SetValue(290)
     c:RegisterEffect(e0)
-    --  Efecto 1: Debe ser Invocado solo por Sincronía
-    local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	--	Efecto 1: No puede ser destruido por batalla
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.synlimit)
-	c:RegisterEffect(e1)
-	--	Efecto 2: No puede ser destruido por batalla
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetCode(EFFECT_IMMUNE_EFFECT)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(function(e,te) return te:GetOwner()~=e:GetOwner() end)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(function(e,te) return te:GetOwner()~=e:GetOwner() end)
+    c:RegisterEffect(e1)
+    --  Efecto 2: Invocar de Modo Especial 1 Monstruo de Sincronía
+    local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetCondition(function(e) return e:GetHandler():IsSynchroSummoned() end)
+    e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
-    --  Efecto 3: Invocar de Modo Especial 1 Monstruo de Sincronía
-    local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e3:SetCondition(function(e) return e:GetHandler():IsSynchroSummoned() end)
-    e3:SetCountLimit(1,{id,1})
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
-    c:RegisterEffect(e3)
     --  Efecto 3: Efectos de secuencia
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0)) 
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
-	e4:SetRange(LOCATION_MZONE) -- Asumiendo que es un monstruo en el campo. Cámbialo si es Mágica/Trampa.
-	e4:SetTarget(s.target)
-	e4:SetOperation(s.operation)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,3)) 
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,{id,2})
+	e3:SetTarget(s.atkth)
+	e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
+    --  Efecto 4: Negar efectos
+    local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,4))
+	e4:SetCategory(CATEGORY_DISABLE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetHintTiming(0,TIMING_ATTACK|TIMING_BATTLE_END)
+    e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetCondition(function() return Duel.IsBattlePhase() end)
+	e4:SetTarget(s.netg)
+	e4:SetOperation(s.neop)
 	c:RegisterEffect(e4)
 end
 s.material={290}
 s.listed_names={290}
 s.synchro_nt_required=1
 s.synchro_tuner_required=1
-    --  *EFECTO 3° 
+    --  *EFECTO 2° 
 function s.exspfilter(c,e,tp)
     return c:IsRace(RACE_WARRIOR|RACE_SPELLCASTER)  and c:IsLevelBelow(8) and c:IsType(TYPE_SYNCHRO) 
         and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
@@ -73,104 +84,41 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		tc:CompleteProcedure()
 	end
 end
-	--  *Efecto 4°
--- Filtros de Condiciones
-function s.syncfilter(c)
-    return c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
-end
-
-function s.negfilter(c)
-    return c:IsFaceup() and not c:IsDisabled()
-end
-
+	--  *Efecto 3°
 function s.atkfilter(c)
     return c:IsFaceup() and c:GetBaseDefense()>0
 end
-
--- Selección de Objetivos / Opciones
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and s.negfilter(chkc) end
-    
-    local b1=Duel.IsExistingTarget(s.negfilter,tp,0,LOCATION_ONFIELD,1,nil)
-    local b2=Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,0,1,nil)
-    
-    if chk==0 then return b1 or b2 end
-    
-    local op=0
-    if b1 and b2 then
-        -- Si ambas son posibles: 0=Negar, 1=ATK, 2=Ambas
-        op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2),aux.Stringid(id,3))
-    elseif b1 then
-        op=Duel.SelectOption(tp,aux.Stringid(id,1))
-    else
-        op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
-    end
-    
-    e:SetLabel(op)
-    
-    -- Manejo dinámico del Target (Solo si es la opción 0 [Negar] o la 2 [Ambas])
-    if op==0 or op==2 then
-        e:SetProperty(EFFECT_FLAG_CARD_TARGET)
-        local ct=Duel.GetMatchingGroupCount(s.syncfilter,tp,LOCATION_MZONE,0,nil)+1
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISABLE)
-        local g=Duel.SelectTarget(tp,s.negfilter,tp,0,LOCATION_ONFIELD,1,ct,nil)
-        Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
-    else
-        e:SetProperty(0)
-    end
-    
-    if op==1 or op==2 then
-        Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,nil,0,tp,LOCATION_MZONE)
+function s.atkth(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,nil,0,tp,LOCATION_MZONE)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local sg=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
+	local c=e:GetHandler()
+	local tc=sg:GetFirst()
+    for tc in aux.Next(sg) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(tc:GetBaseDefense())
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE_START,2)
+		tc:RegisterEffect(e1)
     end
 end
-
--- Resolución del Efecto
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local op=e:GetLabel()
-    
-    -- Operación: Negar (Opción 0 o 2)
-    if op==0 or op==2 then
-        local tg=Duel.GetTargetCards(e)
-        if tg and #tg>0 then
-            for tc in aux.Next(tg) do
-                if tc:IsFaceup() and not tc:IsDisabled() and tc:IsRelateToEffect(e) then
-                    Duel.NegateRelatedChain(tc,RESET_TURN_SET)
-                    
-                    local e1=Effect.CreateEffect(c)
-                    e1:SetType(EFFECT_TYPE_SINGLE)
-                    e1:SetCode(EFFECT_DISABLE)
-                    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-                    tc:RegisterEffect(e1)
-                    
-                    local e1a=e1:Clone()
-                    e1a:SetCode(EFFECT_DISABLE_EFFECT)
-                    e1a:SetValue(RESET_TURN_SET)
-                    tc:RegisterEffect(e1a)
-                    
-                    if tc:IsType(TYPE_TRAPMONSTER) then
-                        local e1b=e1:Clone()
-                        e1b:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-                        tc:RegisterEffect(e1b)
-                    end
-                end
-            end
-        end
-    end
-    -- Romper la cadena en caso de seleccionar ambas opciones
-    if op==2 then
-        Duel.BreakEffect()
-    end
-    -- Operación: Ganar ATK (Opción 1 o 2)
-    if op==1 or op==2 then
-        local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
-        for tc in aux.Next(g) do
-            local e2=Effect.CreateEffect(c)
-            e2:SetType(EFFECT_TYPE_SINGLE)
-            e2:SetCode(EFFECT_UPDATE_ATTACK)
-            e2:SetValue(tc:GetBaseDefense())
-            e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
-            tc:RegisterEffect(e2)
-        end
-    end
+    --  *EFECTO 4°
+function s.netg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and chkc:IsNegatable() end
+	local ct=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsType,TYPE_SYNCHRO),tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+end
+function s.neop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e):Match(Card.IsFaceup,nil)
+	if #tg==0 then return end
+	local c=e:GetHandler()
+	for tc in tg:Iter() do
+		tc:NegateEffects(c,RESET_PHASE|PHASE_END,true)
+	end
 end
